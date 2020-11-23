21d478448ec78382b23c16132e41048e9c8f5e19
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2020-11-23 19:02:57 +0300

    v.1 desktop version almost done

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.querySelector('#forHouse').addEventListener('click', () => {
+	document.querySelector('.header__forHouse').classList.toggle('active');
+  });
+
+document.querySelector('#forOffice').addEventListener('click', () => {
+	document.querySelector('.header__forOffice').classList.toggle('active');
+  });
+
+document.querySelector('#forCar').addEventListener('click', () => {
+	document.querySelector('.header__forCar').classList.toggle('active');
+  });
