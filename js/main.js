4f2ada5ed572fc0baeff44ab1342474f303753ec
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2020-11-25 19:04:37 +0300

    v.1.3 slider added, animation in first block added

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ document.querySelector('#forOffice').addEventListener('click', () => {
 document.querySelector('#forCar').addEventListener('click', () => {
 	document.querySelector('.header__forCar').classList.toggle('active');
   });
+
+  // Слайдер swiper
+
+  var mySwiper = new Swiper('.swiper-container', {
+	
+	loop: true,
+	slidesPerView: 3,
+	spaceBetween: 20,
+	grabCursor: true,
+
+	// Navigation arrows
+	navigation: {
+	  nextEl: '.slider__next',
+	  prevEl: '.slider__prev',
+	}
+  })
